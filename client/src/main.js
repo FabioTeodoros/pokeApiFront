ab0677d7e213711a5cea0b307b09dbc9894bb27f
FabioTeodoros <fabiots@ciandt.com>
2022-07-12 14:52:38 -0300

    reestruturacao pastas e inicio dos componentes

---
+++ client/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './Pokedex.vue'
+import vuetify from '/src/plugins/vuetify'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
